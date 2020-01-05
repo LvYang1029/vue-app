66f9ea600e8277890c4bc5cb1f8b20b3144f9528
LvYang1029 <1210585312@qq.com>
2020-01-05 13:09:35 +0800

    new1

---
+++ src/views/address/List.vue
@@ -0,0 +1,57 @@
+<template>
+    <briup-fulllayout title="常用地址">
+    {{list}}
+        <van-address-list
+            v-model="chosenAddressId"
+            :list="list"
+            :disabled-list="disabledList"
+            disabled-text="以下地址超出配送范围"
+            default-tag-text="默认"
+            @add="onAdd"
+            @edit="onEdit"
+        />
+    </briup-fulllayout>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            chosenAddressId: '1',
+            list:[],
+            disabledList: [
+            {
+                id: '3',
+                name: '王五',
+                tel: '1320000000',
+                address: '浙江省杭州市滨江区江南大道 15 号'
+            }
+        ],
+            address:{},
+        };
+    },
+    methods:{
+        loadAddress(){
+            let id =26;
+            let url ="/address/findByCustomerId?id="+id;
+            get(url).then((response)=>{
+                this.address =response.data;
+                for (let i in address) {
+                    let o = {};
+                    o[i] = address[i];
+                    list.push(o);
+                }
+
+            })
+        },
+        onAdd() {
+            Toast('新增地址');
+        },
+
+        onEdit(item, index) {
+            Toast('编辑地址:' + index);
+        }
+    },
+}
+</script>
+<style scoped>
+</style>
--- src/views/manager/Home.vue
+++ src/views/manager/Home.vue
@@ -3,17 +3,62 @@
     <header class="header">
       <img src="../../assets/home.jpg" alt="">
     </header>
-   
+    <!-- 宫格 -->
+    <!-- {{categories}} -->
+    <!-- {{products}} -->
+   <van-grid :column-num="3">
+  <van-grid-item
+    v-for="value in categories"
+    :key="value.id"
+    :icon="value.icon"
+    :text="value.name"
+  />
+</van-grid>
+<van-grid :column-num="1" icon-size="350px">
+  <van-grid-item
+    v-for="value in products"
+    :key="value.id"
+    :icon="value.photo"
+    :text="value.name"
+  />
+</van-grid>
   </div>
 </template>
 <script>
 import {mapState, mapActions} from 'vuex'
+import {get,post} from '../../http/axios';
 export default {
-  
+  data(){
+    return{
+      categories:[],
+      products:[],
+    };
+  },
   created(){
+    //查询栏目信息
+    this.loadCategories();
+    //查询产品信息
+    this.loadProducts();
   },
   methods:{
-
+    //加载栏目信息
+    loadCategories(){
+      let url = "/category/findAll";
+      get(url).then((response)=>{
+        this.categories = response.data.slice(0,6);
+      })
+    },
+    //加载产品信息
+    loadProducts(){
+      let url = "/product/query";
+      let params={
+        page:0,
+        pageSize:10,
+      };
+      post(url,params).then((response)=>{
+        this.products = response.data.list;
+      });
+    },
   }
 }
 </script>
